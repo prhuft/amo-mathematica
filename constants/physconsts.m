e9be09862cf020f2cf5e8917ae094be24ae48a86
prhuft <gothrowayo@gmail.com>
2021-01-13 22:00:37 -0600

    repo cleanup and directory structure

---
+++ constants/physconsts.m
@@ -0,0 +1,26 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf840
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 e = 1.6*10^-19; (*[C]*)\
+c = 2.9987*10 ^8; (*[m/s]*)\
+\
+a0 = 5.29*10^-11;(*[m]*)\
+\\[Mu]0 = \
+  4 \\[Pi] * 10^-7; (*[N/A^2,T\\[CenterDot]m/A]*)\
+\\[Epsilon]0 = \
+  8.85*10^-12;(*[A^2s^4/m^3kg]*)\
+\\[Mu]B = (e \\[HBar])/(2 me);(*[J/T]*)\
+\
+kB = 1.3807*10^-23; (*[J/K]*)\
+\\[HBar] = \
+  1.05*10^-34;(*[J\\[CenterDot]s]*)\
+h = 2 \\[Pi] \\[HBar];\
+EHartree = 2*(13.6);(*[eV]*)\
+me = 9.11*10^-31;(*[kg]*)\
+\
+mp = 1.67*10^-27;(*[kg]*)\
+\\[Alpha] = 1/137;}
---
+++ constants/rbconsts.m
@@ -0,0 +1,29 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf840
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 INuc = 3/2;(* nuclear spin *)\
+mRb = 1.4192261*10^-25; (*[kg]*)\
+\
+gS = 2.00023; (*Lande g, spin*)\
+\
+gL = 1 ; (*This might be specifically for L=0*)\
+gI = -0.000995;\
+D2MatElem = 3.584*10^-29;(*<J=1/2||er||J'=3/2>[C*m]*)\
+\
+D1MatElem = 2.537*10^-29;(*<J=1/2||er||J'=1/2>[C*m]*)\
+\\[Lambda]D1 = \
+  794.97885098*10^-9;(*[m]*) \
+\\[Lambda]D2 = 780.24120968613*10^-9;(*[m]*)\
+\\[Nu]D1 = \
+  384.230484468562; (*[THz]*)\
+\\[Nu]D2 = \
+  377.10746354 ;(*[THz]*)\
+\\[Nu]HF = \
+  6.83468261090429 ;(*[Ghz]*)\
+\\[CapitalGamma]D2 = \
+  2 \\[Pi] 6.0659*10^6 ;(*[rad Hz]*)\
+EHF = h 10^9 \\[Nu]HF;(*[J]*)}
