adb384dea3a204ffdf48a571e4771cc448e9ab4b
prhuft <gothrowayo@gmail.com>
2021-08-10 09:40:53 -0500

    d1 line consts

--- constants/rbconsts.m
+++ constants/rbconsts.m
@@ -6,11 +6,14 @@ gL = 1;
 gI = -0.000995;
 D2MatElem = 3.584*10^-29;(*<J=1/2||er||J'=3/2>[C*m]*)
 D1MatElem = 2.537*10^-29;(*<J=1/2||er||J'=1/2>[C*m]*)
+IsatD2 = 2.5033; (*effective far-detuned. mW/cm^2*)
+IsatD1 = 4.4845; (*effective far-detuned. mW/cm^2*)
 \[Lambda]D1 = 794.97885098*10^-9;(*[m]*) 
 \[Lambda]D2 = 780.24120968613*10^-9;(*[m]*)
 \[Nu]D1 = 377.10746354; (*[THz]*)
 \[Nu]D2 = 384.230484468562;(*[THz]*)
 \[Nu]HF = 6.83468261090429 ;(*[Ghz]*)
 \[CapitalGamma]D2 = 2 \[Pi] 6.0659*10^6 ;(*[rad Hz]*)
+\[CapitalGamma]D1 = 2 \[Pi] 5.7468*10^6 ;(*[rad Hz]*)
 EHF = h 10^9 \[Nu]HF;(*[J]*)
 
